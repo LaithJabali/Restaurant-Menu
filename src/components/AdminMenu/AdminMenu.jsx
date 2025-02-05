680dbab532727bc4c616a09ef56e4d58ae19e7dd
LaithJabali <laithkingjabali@gmail.com>
2025-02-05 15:02:49 +0200

    updates on admin page

--- src/components/AdminMenu/AdminMenu.jsx
+++ src/components/AdminMenu/AdminMenu.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import styles from './AdminMenu.module.css';
-import LocationDropdown from './LocationDropdown';
-import MenuList from '../MenuList/MenuList';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useLanguage } from "../LanguageButton/LanguageContext";
+import { useLanguage } from '../LanguageButton/LanguageContext';
+import Logo from '../Logo/Logo';
+import MenuList from '../MenuList/MenuList';
+import styles from './AdminMenu.module.css';
+
 const AdminMenu = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   const { language } = useLanguage();
-const goToHomePage = () => {
-  navigate('/');
-};  
+
+  const goToHomePage = () => {
+    navigate('/');
+  };
+
   return (
     <div className={styles.page}>
-            <button className={styles.addDish} onClick={goToHomePage}> {language === "English" ? 'Go to Home Page' : 'الصفحة الرئيسية'}</button>
-
-     <MenuList isAdmin={true}/>
+      <div className={styles.header}>
+        <Logo />
+        <button className={styles.addDish} onClick={goToHomePage}>
+          {language === 'English' ? 'Go to Home Page' : 'الصفحة الرئيسية'}
+        </button>
+      </div>
+      <MenuList isAdmin={true} />
     </div>
   );
 };
